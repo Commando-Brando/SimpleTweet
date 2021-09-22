aee52b5d26b18424f726ee06e0d40333f0510602
Commando-Brando <branmuller@gmail.com>
2021-09-22 18:35:26 -0500

    configured restful template for twitter API & started building timeline scene

--- app/src/main/java/com/codepath/apps/restclienttemplate/LoginActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.codepath.apps.restclienttemplate;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -44,8 +45,8 @@ public class LoginActivity extends OAuthLoginActionBarActivity<TwitterClient> {
 	@Override
 	public void onLoginSuccess() {
 		Log.i("sdsdad", "login success");
-		// Intent i = new Intent(this, PhotosActivity.class);
-		// startActivity(i);
+		Intent i = new Intent(this, TimelineActivity.class);
+		startActivity(i);
 	}
 
 	// OAuth authentication flow failed, handle the error
