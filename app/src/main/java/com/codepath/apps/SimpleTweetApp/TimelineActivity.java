509c7a9615efdf3f5143b8bbfb8e6700079bbdd4
Commando-Brando <branmuller@gmail.com>
2021-10-05 20:39:28 -0500

    finished user stories

--- app/src/main/java/com/codepath/apps/SimpleTweetApp/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/SimpleTweetApp/TimelineActivity.java
@@ -1,5 +1,6 @@
 package com.codepath.apps.SimpleTweetApp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.DividerItemDecoration;
@@ -7,14 +8,19 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.codepath.apps.SimpleTweetApp.models.Tweet;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.parceler.Parcels;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,6 +36,7 @@ import okhttp3.Headers;
 public class TimelineActivity extends AppCompatActivity {
 
     public static final String TAG = "TimelineActivity";
+    private final int REQUEST_CODE = 20;
 
     TwitterClient client;
     RecyclerView rvTweets;
@@ -119,12 +126,44 @@ public class TimelineActivity extends AppCompatActivity {
         }, tweets.get(tweets.size() - 1).id);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(item.getItemId() == R.id.compose){
+            // navigate to the compose activity
+            Intent intent = new Intent(this, ComposeActivity.class);
+            startActivityForResult(intent, REQUEST_CODE);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == REQUEST_CODE && resultCode == RESULT_OK){
+            // get data from the intent (tweet object)
+            Tweet tweet = Parcels.unwrap(data.getParcelableExtra("tweet"));
+            // update recycler view with the tweet
+            // modify data source of the tweets
+            tweets.add(0, tweet);
+            // update the adapter
+            adapter.notifyItemInserted(0);
+            rvTweets.smoothScrollToPosition(0);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
     /*
-        populates our RV timeline with tweets
-        makes calls to the client (TwitterClient.java object)
-        that allow it retrieve Json data from the twitter API
-        we also refresh out adapter here when we use the swipe down feature via SwipeContainer
-     */
+            populates our RV timeline with tweets
+            makes calls to the client (TwitterClient.java object)
+            that allow it retrieve Json data from the twitter API
+            we also refresh out adapter here when we use the swipe down feature via SwipeContainer
+         */
     private void populateHomeTimeline() {
         client.getHomeTimeline(new JsonHttpResponseHandler() {
             @Override
